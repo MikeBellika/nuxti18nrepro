0f96044114a2835068ced0ce05e2b5504987b03d
Mike Bellika <mb@moxso.com>
2024-05-17 10:22:56 +0200

    Initial commit

---
+++ app.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+const { setLocale, locale } = useI18n();
+function switchLanguage() {
+  setLocale(locale.value == 'en' ? 'fr' : 'en');
+}
+</script>
+<template>
+  <div>
+    {{ $t('welcome') }}
+    <button @click="switchLanguage">Switch language</button>
+  </div>
+</template>
